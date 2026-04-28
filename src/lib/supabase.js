9b944d2ddef0a58b8c1674c4e9e9f1d2037af682
Etwin Feoh <xcode6six@gmail.com>
2026-04-29 01:44:48 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://crzymkebjvqhqlvjhrwb.supabase.co/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyenlta2VianZxaHFsdmpocndiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MzkzNjIsImV4cCI6MjA5MDQxNTM2Mn0.zqpbjuGH7E0AOImYZN73vo3OjmwoyRPH7fX9U0JklyA';
+const supabaseUrl = 'ganti_url';
+const supabaseKey = 'ganti_key';
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
